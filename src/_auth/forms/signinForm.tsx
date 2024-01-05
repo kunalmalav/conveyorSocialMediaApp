1dcabacf24b9e6bec581d49964350576402dcdbc
Abhinay Pratap <abhinaypratap@icloud.com>
2024-01-05 17:44:43 +0530

    do more

---
+++ src/_auth/forms/signinForm.tsx
@@ -0,0 +1,9 @@
+const SigninForm = () => {
+    return(
+        <div>
+          <Button>Click me</Button>
+        </div>
+    )
+}
+
+export default SigninForm
---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,9 @@
+const Rootnpm dnLayout = () => {
+    return(
+        <div>
+          <Button>Click me</Button>
+        </div>
+    )
+}
+
+export default RootLayout
